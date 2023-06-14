4f8b99ec8a4bf80abf54242eefad50fa4c70ffda
Sam Ridding <sam@sams-air.home>
2023-06-14 21:42:10 +0100

    Add mix model

--- mixes/models.py
+++ mixes/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from embed_video.fields import EmbedVideoField
 
-# Create your models here.
+
+class Mix(models.Model):
+
+    title = models.CharField(max_length=200, blank=False)
+    image = models.ImageField(
+        upload_to='images/', blank=False
+    )
+    content = models.TextField(blank=False)
+    audio = EmbedVideoField(blank=True)
+    draft = models.BooleanField()
+    posted_on = models.DateTimeField(auto_now_add=True)
+    edited_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-posted_on']
+
+    def __str__(self):
+        return f'{self.id} {self.title}'
