6af3504aa577837eec256e9dab8bb5273c06f20c
Sam Ridding <sam.ridding@hotmail.co.uk>
2024-01-29 20:52:24 +0000

    Update allowed hosts

--- backtofront/settings.py
+++ backtofront/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['web-production-3d81.up.railway.app', '127.0.0.1']
+ALLOWED_HOSTS = ['.railway.app', '127.0.0.1']
 
 
 # Application definition
