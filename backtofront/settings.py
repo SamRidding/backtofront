46f2522ae95f60e839d9f8db4f682b5f7034f916
Sam Ridding <sam.ridding@hotmail.co.uk>
2024-02-04 20:35:13 +0000

    Update allows hosts

--- backtofront/settings.py
+++ backtofront/settings.py
@@ -28,9 +28,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['https://backtofront-production.up.railway.app/', '127.0.0.1']
+ALLOWED_HOSTS = ['backtofront-production.up.railway.app', '127.0.0.1']
 CSRF_TRUSTED_ORIGINS = ['https://*.railway.app']
 
 # Application definition
